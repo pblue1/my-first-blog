46132219db85e44ab7c89f44da1e024ac86e723c
SungBae Park <pblue11@gmail.com>
2018-10-23 17:17:24 +0900

    Updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.contrib.auth.views import login,logout
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
+	url(r'^accounts/login/$',login, name='login'),
+	url(r'^accounts/logout/$',logout, name='logout',
+		kwargs={'next_page': '/'}),
 ]
